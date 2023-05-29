4e6fde4531fef55ae4bac6c4622cb729df2f581e
Marcelo Salinas <marcelofabiansalinas@gmail.com>
2023-05-29 20:15:50 -0300

    primer-commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/header"
+import './App.css'
+
+
+function App() {
+
+
+  return (
+
+      <div>
+        <Header />
+      </div>
+
+
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+
+    return (
+            <header className="header">
+            <div className="header__box">
+                <h1 className="header__title">PeriExport</h1>
+
+                <nav className="header__nav">
+                    <a className="header__a" href="#">Inicio</a>
+                    <a className="header__a" href="#">Productos</a>
+                    <a className="header__a" href="#">Contacto</a>
+                </nav>
+            </div>
+        </header>
+    )
+} 
+export default Header
